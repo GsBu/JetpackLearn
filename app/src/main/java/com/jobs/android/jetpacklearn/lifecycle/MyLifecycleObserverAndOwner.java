4ffc6691cc194b7358bbcb4ad40ce88b8e6260b3
GsBu <1129652584@qq.com>
2022-01-17 10:05:20 +0800

    学习Lifecycle，解决git冲突

--- app/src/main/java/com/jobs/android/jetpacklearn/lifecycle/MyLifecycleObserverAndOwner.java
+++ app/src/main/java/com/jobs/android/jetpacklearn/lifecycle/MyLifecycleObserverAndOwner.java
@@ -2,9 +2,11 @@ package com.jobs.android.jetpacklearn.lifecycle;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LifecycleRegistry;
 import androidx.lifecycle.OnLifecycleEvent;
 
 /**
@@ -16,44 +18,62 @@ import androidx.lifecycle.OnLifecycleEvent;
  *          先调用ON_PAUSE、ON_STOP、ON_DESTROY、ON_ANY，再调用生命周期的onPause、onStop、onDestroy
  *          2、加注解的方法中，可以加LifecycleOwner参数。
  */
-public class MyLifecycleObserver implements LifecycleObserver {
-    public MyLifecycleObserver(){
+public class MyLifecycleObserverAndOwner implements LifecycleObserver, LifecycleOwner{
 
+    private LifecycleRegistry mLifecycleRegistry;
+
+    public MyLifecycleObserverAndOwner(){
+        mLifecycleRegistry = new LifecycleRegistry(this);
+        getLifecycle().addObserver(new MyLifecycleObserver());
     }
 
     @OnLifecycleEvent(value = Lifecycle.Event.ON_CREATE)
-    public void init(){
+    public void myCreate(){
         Log.e("aaaa","ON_CREATE被调用，执行初始化");
+        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE);
     }
 
     @OnLifecycleEvent(value = Lifecycle.Event.ON_START)
-    public void init1(){
+    public void myStart(){
         Log.e("aaaa","ON_START被调用");
+        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START);
     }
 
     @OnLifecycleEvent(value = Lifecycle.Event.ON_RESUME)
-    public void init2(){
+    public void myResume(){
         Log.e("aaaa","ON_RESUME被调用");
+        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME);
     }
 
     @OnLifecycleEvent(value = Lifecycle.Event.ON_ANY)
-    public void bbb(LifecycleOwner owner){
+    public void myAny(LifecycleOwner owner){
         Log.e("aaaa","任何时候都执行 是否已经start了：" +
                 owner.getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED));
+        // TODO: 2022/1/14 这句会报错，暂不知道原因
+        //mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_ANY);
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
-    public void a(){
+    public void myPause(){
         Log.e("aaaa","ON_PAUSE被调用");
+        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE);
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
-    public void aa(){
+    public void myStop(){
         Log.e("aaaa","ON_STOP被调用");
+        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_STOP);
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    public void aaa(){
+    public void myDestroy(){
         Log.e("aaaa","ON_DESTROY被调用");
+        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY);
+    }
+
+    @NonNull
+    @Override
+    public Lifecycle getLifecycle() {
+        return mLifecycleRegistry;
     }
 }
