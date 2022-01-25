07692d7a009071a1796eedd267d0eaf18d343a74
GsBu <1129652584@qq.com>
2022-01-25 15:57:32 +0800

    学习LiveData，基础使用

--- app/src/main/java/com/jobs/android/jetpacklearn/livedata/LiveDataTestActivity.java
+++ app/src/main/java/com/jobs/android/jetpacklearn/livedata/LiveDataTestActivity.java
@@ -1,16 +1,67 @@
 package com.jobs.android.jetpacklearn.livedata;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 
 import com.jobs.android.jetpacklearn.R;
 
 public class LiveDataTestActivity extends AppCompatActivity {
+    private static final String TAG = LiveDataTestActivity.class.getSimpleName();
+
+    private MutableLiveData<String> mLiveData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_data_test);
+
+        mLiveData = new MutableLiveData<String>();
+        mLiveData.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                Log.e(TAG, "onChanged：" + s);
+            }
+        });
+        Log.e(TAG, "onCreate");
+        mLiveData.setValue("onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart");
+        mLiveData.setValue("onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume");
+        mLiveData.setValue("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause");
+        mLiveData.setValue("onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop");
+        mLiveData.setValue("onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy");
+        mLiveData.setValue("onDestroy");
     }
 }
